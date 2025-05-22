18706e977d46fc5a0821c4d5b5067942c6a19e66
Kimeragb <gabriel@smack.sc.ug>
2025-05-23 00:22:08 +0300

    set_up

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/WorkinV_CommunityBridges/' // 👈 change this to your repo name
-})
